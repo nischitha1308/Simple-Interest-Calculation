318429594f759dcebf7ceee8a8d3ccb59b5f6185
B.Nischitha <bnischithareddy@gmail.com>
2025-05-25 15:54:40 +0530

    Add files via upload

--- Interest Rate calculator/script.js
+++ Interest Rate calculator/script.js
@@ -1,11 +1,11 @@
 const principle=document.getElementById("amount");
-const rate=document.getElementById("rate");
+const Rate=document.getElementById("rate");
 const time=document.getElementById("year");
-const totall=document.getElementById("total");
-const btn=document.getElementById("bttn");
+const totall=document.getElementById('total');
+const btn=document.getElementById('bttn');
 function calculateresult(){
     const p=principle.value;
-    const r=rate.value;
+    const r=Rate.value;
     const t=time.value;
     const tm=eval(p*t*r/100);
     totall.innerText=tm;
